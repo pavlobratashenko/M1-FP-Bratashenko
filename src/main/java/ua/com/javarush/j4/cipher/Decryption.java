d59ad68efee034b7e712f6fb6118a45ef8d83f70
Pavlo Bratashenko <bratashenkopavlo@gmail.com>
2026-06-14 16:50:25 +0300

    Розробка метода дешифрування

--- src/main/java/ua/com/javarush/j4/cipher/Decryption.java
+++ src/main/java/ua/com/javarush/j4/cipher/Decryption.java
@@ -5,18 +5,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Decryption {
+
     private final List<Character> ALPHABET = Arrays.asList('а', 'б', 'в',
             'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
             'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
             ':', '!', '?', ' ');
     private final int ALPHABET_SIZE = ALPHABET.size();
 
-    public  ArrayList<Character> shift(ArrayList<Character> list, int key) {
-        ArrayList<Character> charListOut = new ArrayList<>();
 
-        for (int i = 0; i < list.size(); i++) {
-            charListOut.add(ALPHABET.get(ALPHABET.indexOf(list.get(i)) + key  % ALPHABET_SIZE));
+    public ArrayList<Character> shiftBack(ArrayList<Character> listIn, int key,
+                                          ArrayList<Character> listOut) {
+
+        for (int i = 0; i < listIn.size(); i++) {
+            if  ((ALPHABET.get(ALPHABET.indexOf(listIn.get(i)) - key) < 0)) {
+                int temp = ALPHABET.size() - (ALPHABET.get(ALPHABET.indexOf(listIn.get(i)) - key));
+                listOut.add(ALPHABET.get(temp));
+            }else {
+                listOut.add(ALPHABET.get(ALPHABET.indexOf(listIn.get(i)) - key));
+            }
         }
-        return charListOut;
+        return listOut;
     }
 }
+
